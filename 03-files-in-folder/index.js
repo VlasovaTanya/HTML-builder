33698e0b3c5a51e25825d7a5bed8d46d2feb162c
VlasovaTanya <miss.merizy@gmail.com>
2022-05-24 00:03:43 +0300

    feat: solution 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const { stdout } = process;
+
+filePath = path.join(__dirname, 'secret-folder')
+
+
+fs.readdir (filePath, {withFileTypes: true}, (err, files) => {
+    if (err) console.log(err);
+  else {
+    files.forEach(file => {
+      if(file.isFile()){
+        let fileName = file.name.split('.')[0]
+        let rasshir = path.extname(file.name)
+        let size = 0
+        fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
+          if(err){
+            console.log(err)
+          }else{
+            size += stats.size / 1024
+          }
+          stdout.write(`${fileName} - ${rasshir} - ${size}kb\n`)
+})
+      }
+      });
+    }
+    });
+
+
+
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const path = require('path');
+const promises = require('fs/promises');
+const fs = require('fs');
+
+async function copyFiles(params) {
+
+    await fs.promises.rm(path.join(__dirname, 'files-copy'), {force: true, recursive: true });
+
+    await fs.promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+    const dir = path.join(__dirname, 'files');
+
+    const files = await fs.promises.readdir(dir, {withFileTypes: true});
+
+    for (let file of files) {   
+
+        const fileInfoOld = path.join(__dirname, 'files', file.name);
+        const fileInfoNew = path.join(__dirname, 'files-copy', file.name);
+
+        await fs.promises.copyFile(fileInfoOld, fileInfoNew);
+    };
+
+};
+
+
+
+try {
+    copyFiles (); 
+} catch (error) {
+    stderr.write(`Error: ${error}`);
+}
 
